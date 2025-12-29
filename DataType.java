c4ccb9933c0eea9567f4fe12b433ad3ae6deeea6
Akshaya-akv <akshaya.akv33@gmail.com>
2025-12-29 12:04:26 +0530

    Add files via upload

---
+++ DataType.java
@@ -0,0 +1,17 @@
+public class DataType {
+    public static void main(String[] args) {
+
+        int age = 24;
+        double salary = 30000.00;
+        char grade = 'A';
+        
+        boolean isJobConfirmed = true; 
+        String message = "status of application";
+
+        System.out.println("Message: " + message);
+        System.out.println("Age: " + age); 
+        System.out.println("Salary: " + salary); 
+        System.out.println("Grade: " + grade);
+        System.out.println("Is job confirmed: " + isJobConfirmed); 
+    }
+}
